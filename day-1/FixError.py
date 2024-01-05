f80970996210b7faf9faea674980bfe058ef9520
Gayheart2506 <107317418+Gayheart2506@users.noreply.github.com>
2024-01-05 10:08:47 +0000

    fix the code with errors on string concatenation

---
+++ day-1/FixError.py
@@ -0,0 +1,14 @@
+# Fix the code below. 
+
+# print(Day 1 - String Manipulation")
+# print("string Concatenation is done with the "+" sign")
+#   print('eg. print("Hello " + "world")')
+# print(("new lines can be created with a backslash and n.")
+
+
+# Solution to fixing the code above. 
+
+print("Day-1 string Manipulation")
+print("string Concatenation is done with the '+' sign")
+print("eg. print('Hello ' + 'world')")
+print("New lines can be created with a backslash and n\n")
