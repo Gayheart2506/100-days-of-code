4c448c3d31f6f72740bd9a80945e1f1e102d544d
Gayheart2506 <107317418+Gayheart2506@users.noreply.github.com>
2024-01-09 07:41:08 +0000

    Operators and how they work

--- day-1/main.py
+++ day-1/main.py
@@ -3,3 +3,10 @@ print("Hello world!\nHello world!")
 print("Hello " + "Gayheart " + "Ayoo")
 print("Hello" +  "Gayheart")
 print("Hello" + "Ayoo")
+
+# Operators and Variables and how they work
+
+number1 = 10 
+number2 = 20
+final_answer = number1 / number2
+print(final_answer)
