bc0654818077024b067480deef97d0cff0d1e6c2
Gayheart2506 <107317418+Gayheart2506@users.noreply.github.com>
2024-01-09 07:41:44 +0000

    data types and their uses

---
+++ day-1/datatypes.py
@@ -0,0 +1,13 @@
+# Data types in python
+
+name = "Ayoo Gayheart" # str
+age = 23 # int
+height = 6.3 # float
+can_vote = True # boolean
+
+# the type keyword will show which data type each variable represents.
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(can_vote))
