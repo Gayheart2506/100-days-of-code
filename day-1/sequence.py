8f2ea250ed59bedb8427b836f2723ee2d2f6a41a
Gayheart2506 <107317418+Gayheart2506@users.noreply.github.com>
2024-01-10 07:03:10 +0000

    lists, sets, turples and how they work.

---
+++ day-1/sequence.py
@@ -0,0 +1,24 @@
+# list of names
+names = ["Gayheart", "Alice", "Princess", "Belinda", "Marvin"]
+
+print(names[4])
+print(names)
+
+names.append("Draco")
+print(names)
+
+words = "Wonderful"
+print(words[0])
+print(words[4])
+
+# set of items
+s = set()
+s.add("hello")
+s.add("Love")
+print(s)
+
+# turples
+cordinateX = 100.1
+cordinateY = 200.2
+location = (cordinateX, cordinateY)
+print(location)
