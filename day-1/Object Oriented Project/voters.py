0d201efaeddb6f164a960397d11c91767a74403d
Gayheart2506 <107317418+Gayheart2506@users.noreply.github.com>
2024-01-12 11:58:08 +0000

    a folder containing a voters verification project in oop

---
+++ day-1/Object Oriented Project/voters.py
@@ -0,0 +1,21 @@
+class Person():
+    def __init__(self, name, age):
+        self.age = age
+        self.name = name
+    
+
+    def qualified_voter(self):
+        if self.age >= 18:
+            return True
+        else:
+            return False
+
+print("*********** Welcome to the Voters Identifier Program ************\n")
+
+person = Person(input("Enter Name: "), int(input("Qualified to vote ? Enter Age please: ")))
+
+qualified = person.qualified_voter()
+if qualified:
+    print(f"Hello {person.name}, You are Qualified to vote")
+else:
+    print(f"Oops, {person.name}, You are not eligible to vote at the age of {person.age}.")
