b77ec355c2050a7a215738c1cf42696b0de5ad26
Gayheart2506 <107317418+Gayheart2506@users.noreply.github.com>
2024-01-13 17:11:28 +0000

    a nicknaming program using input and string concatenations

---
+++ day-1/inputt.py
@@ -0,0 +1,6 @@
+print("******** Welcome to the Nickname Generator program **********\n")
+
+city = input("Which city do you live in ?\n")
+pet = input("What is the name of your pet ?\n")
+
+print("Your Nickname should be " + city + " " + pet)
