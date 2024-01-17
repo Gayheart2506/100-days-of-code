427723623be9f6b2f446bbec7e5432e9f4ac9e6a
Gayheart2506 <107317418+Gayheart2506@users.noreply.github.com>
2024-01-17 09:16:24 +0000

    some codes on the data types used in python and how they work

---
+++ day-2/datatypes.py
@@ -0,0 +1,20 @@
+# string manipulation 
+
+print("123" + "456")
+
+# subscripting 
+print("Hello"[0])
+
+# Integers 
+
+print(123 + 456)
+number = 123_353_342
+print(number)
+
+# float 
+pi = 3.14159
+print(pi)
+
+# boolean 
+print(True)
+print(False)
