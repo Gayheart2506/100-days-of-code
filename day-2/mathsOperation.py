079c7a713f9291ff33aa93e16660164f295f4aa4
Gayheart2506 <107317418+Gayheart2506@users.noreply.github.com>
2024-01-21 22:29:31 +0000

    how some of the main math operations work

---
+++ day-2/mathsOperation.py
@@ -0,0 +1,14 @@
+"""
+Operands and maths operations used in 
+python and how BODMAS works when the compiler
+tries to make complicated calculation
+"""
+
+print(f"Addition : {3 + 5}")
+print(f"Substraction : {5 - 2}")
+print(f"Multiplication : {5 * 3}")
+print(f"Exponential power : {5 ** 5}")
+print(f"Division : {8 / 3}")
+print(8//3)
+print(round(8/5, 2))
+print(f" BODMAS : {5+3+5-10+(5/7)+8-2}")
